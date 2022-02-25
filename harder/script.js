afd930255eac6fefc3c03bbc444f2241f67e89e2
Egor Apaniuk <egorzeynos@gmail.com>
2022-02-25 11:05:28 +0300

    assigment complete. works well

---
+++ harder/script.js
@@ -0,0 +1,25 @@
+
+(function () {
+
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim", "Egor"];
+
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].toLowerCase().charAt(0);
+    // console.log(firstLetter);
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+
+})();
+
+
+
+
+
+
